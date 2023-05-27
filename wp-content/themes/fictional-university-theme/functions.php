7a0dba5701032c80c5f2084fd68a5450e48ef211
Flamurih <flamurhoxha1998@gmail.com>
2023-05-27 17:06:15 +0200

    Events Post Type

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,10 +1,20 @@
 <?php
 
 function university_files() {
-  wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=yourkeygoeshere', NULL, '1.0', true);
+  wp_enqueue_script('axios', '//cdn.jsdelivr.net/npm/axios/dist/axios.min.js', NULL, '1.0', true);
+  wp_enqueue_script('glidejs', '//cdn.jsdelivr.net/npm/@glidejs/glide', NULL, '1.0', true);
+
+  wp_enqueue_script('main-university-js', get_theme_file_uri('/scripts.js'), array('jquery'), '1.0', true);
   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+
+  wp_localize_script('main-university-js', 'universityData', array(
+    'root_url' => get_site_url(),
+    'nonce' => wp_create_nonce('wp_rest')
+  ));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
@@ -14,3 +24,22 @@ function university_features() {
 }
 
 add_action('after_setup_theme', 'university_features');
+
+function university_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            ));
+  }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
