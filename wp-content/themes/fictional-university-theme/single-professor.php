7dc4f3cdaa36407da7f0621dcb7f5e43610183a2
Flamurih <flamurhoxha1998@gmail.com>
2023-06-14 17:42:41 +0200

    Professor Post Type Added

--- wp-content/themes/fictional-university-theme/single-professor.php
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -3,13 +3,20 @@
   get_header();
 
   while(have_posts()) {
-    the_post();
-    pageBanner();
-     ?>
-    
+    the_post(); ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner'] ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p><?php the_field('page_banner_subtitle') ?></p>
+        </div>
+      </div>  
+    </div>
 
     <div class="container container--narrow page-section">
           
+
       <div class="generic-content">
         <div class="row group">
 
@@ -18,48 +25,6 @@
           </div>
 
           <div class="two-thirds">
-            <?php
-
-              $likeCount = new WP_Query(array(
-                'post_type' => 'like',
-                'meta_query' => array(
-                  array(
-                    'key' => 'liked_professor_id',
-                    'compare' => '=',
-                    'value' => get_the_ID()
-                  )
-                )
-              ));
-
-              $existStatus = 'no';
-
-              if (is_user_logged_in()) {
-                $existQuery = new WP_Query(array(
-                  'author' => get_current_user_id(),
-                  'post_type' => 'like',
-                  'meta_query' => array(
-                    array(
-                      'key' => 'liked_professor_id',
-                      'compare' => '=',
-                      'value' => get_the_ID()
-                    )
-                  )
-                ));
-
-                if ($existQuery->found_posts) {
-                  $existStatus = 'yes';
-                }
-              }
-
-              
-
-            ?>
-
-            <span class="like-box" data-like="<?php echo $existQuery->posts[0]->ID; ?>" data-professor="<?php the_ID(); ?>" data-exists="<?php echo $existStatus; ?>">
-              <i class="fa fa-heart-o" aria-hidden="true"></i>
-              <i class="fa fa-heart" aria-hidden="true"></i>
-              <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
-            </span>
             <?php the_content(); ?>
           </div>
 
@@ -83,6 +48,8 @@
       ?>
 
     </div>
+    
+
     
   <?php }
 
